04764274219f33c4152f27663cae5e03e0cdfea9
Alkon <sasha12345333.ru@bk.ru>
2025-11-08 10:36:37 +0700

    1. Убрал user из ТЗ.md 2. Добавил библиотеку классов Domain и Приложение WPF UI 3. В Domain добавлены 4 класса

---
+++ Domain/PlaylistTrack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class PlaylistTrack
+    {
+        public int Id { get; set; }
+
+        public int PlaylistId { get; set; }
+        public Playlist Playlist { get; set; }
+
+        public int AudioFileId { get; set; }
+        public AudioFile AudioFile { get; set; }
+    }
+}
